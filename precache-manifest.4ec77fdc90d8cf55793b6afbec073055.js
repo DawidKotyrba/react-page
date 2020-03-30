006839fa961f583e52c4e00197e55d132cdd405f
Dawid Kotyrba <dawid.kotyrba@interia.pl>
2020-03-30 16:00:05 +0200

    Updates

---
+++ precache-manifest.4ec77fdc90d8cf55793b6afbec073055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3dcaa5725452cf6c1340eac27e4d29",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "213d0fdcfcb9e20469e0",
+    "url": "/react-page/static/css/main.1f424cbf.chunk.css"
+  },
+  {
+    "revision": "50e0dec72fb01d439a69",
+    "url": "/react-page/static/js/2.f89cf218.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-page/static/js/2.f89cf218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213d0fdcfcb9e20469e0",
+    "url": "/react-page/static/js/main.19c3e8f7.chunk.js"
+  },
+  {
+    "revision": "9c310d76ecfb726ac524",
+    "url": "/react-page/static/js/runtime-main.64c8423d.js"
+  }
+]);
